ffed42fbf2f276f54119385fe186e82ddb947b0a
Adrien N. F. <iamnautilusjungle@gmail.com>
2023-10-20 15:00:21 +0000

    add tests for ResponseBuilder

---
+++ tests/unit/Interpreter/ResponseBuilderTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+use AdrienNf\BrainfuckInterpreter\Interpreter\Response;
+use AdrienNf\BrainfuckInterpreter\Interpreter\ResponseBuilder;
+use PHPUnit\Framework\TestCase;
+
+final class TestableResponseBuilder extends ResponseBuilder {
+    public function getData(): array {
+        return $this->data;
+    }
+}
+
+final class ResponseBuilderTest extends TestCase
+{
+    public function testAdd()
+    {
+        $builder = new TestableResponseBuilder();
+        $builder->add(72);
+        $builder->add(101);
+
+        $this->assertSame([72, 101], $builder->getData());
+    }
+
+    public function testBuild()
+    {
+        $builder = new TestableResponseBuilder();
+        $builder->add(72);
+        $builder->add(101);
+        $builder->add(108);
+
+        $response = $builder->build();
+        $this->assertSame([], $builder->getData());
+
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertSame([72, 101, 108], $response->getData());
+    }
+}
